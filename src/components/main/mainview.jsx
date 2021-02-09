3a849e4b77c0f39d65f32faff06eab4e85df5b58
shamoda <it19161648@my.sliit.lk>
2021-02-09 20:57:35 +0530

    Download controllers added.

--- src/components/main/mainview.jsx
+++ src/components/main/mainview.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/sort-comp */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/no-unused-state */
@@ -24,46 +25,89 @@ class Mainview extends Component {
       totalSize:''
     }
 
-    this.download = this.download.bind(this);
+    this.btnClick = this.btnClick.bind(this);
   }
 
   inputChange = event =>{
     this.setState({
         [event.target.name] : event.target.value
     });
+
+    // if(event.target.value  Check is this valid or not)
+    // {
+    //   video = ytdl(this.state.urlTxt, { format: 'mp4' });
+    // }
+
+    console.log(event.target.value)
+
   };
 
-  download(url){
-    // console.log(url)
 
-    // server.downloadVideo(url)
-    let output = path.resolve(__dirname, 'video.mp4');
-    let video = ytdl(url, { format: 'mp4' });
+  output = path.resolve(__dirname, 'video.mp4');
+  
+  video = ytdl(this.state.urlTxt, { format: 'mp4' });
+
+
+
+  stop(btn) {
+    console.log(btn)
+    if(this.video.isPaused){
+      this.video.resume();
+    }
+
+    this.video.destroy();
+
+      if (fs.existsSync(this.output)) {
+        fs.unlink(this.output, (err) => {
+            if (err) {
+                alert("An error ocurred updating the file" + err.message);
+                console.log(err);
+                return;
+            }
+            console.log("File succesfully deleted");
+        });
+      }else{
+        alert("This file doesn't exist, cannot delete");
+      }
+  }
+
+
+
+  btnClick(btn){
+    console.log(btn)
+
     let starttime;
-    video.pipe(fs.createWriteStream(output));
-    video.once('response', () => {
-      starttime = Date.now();
-    });
-    video.on('progress', (chunckLength, downloaded, total) => {
-      const percent = downloaded / total;
-      const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
-      const estimatedDownloadTime = (downloadedMinutes / percent) - downloadedMinutes;
-      this.setState({
-        progress: (percent * 100).toFixed(2),
-        estTime: estimatedDownloadTime.toFixed(2),
-        downloadedSize: (downloaded / 1024 / 1024).toFixed(2),
-        totalSize: (total / 1024 / 1024).toFixed(2)
+
+      this.video.pipe(fs.createWriteStream(this.output));
+
+       this.video.once('response', () => {
+        starttime = Date.now();
       });
-      readline.cursorTo(process.stdout, 0);
-      process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
-      process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)\n`);
-      process.stdout.write(`running for: ${downloadedMinutes.toFixed(2)}minutes`);
-      process.stdout.write(`, estimated time left: ${estimatedDownloadTime.toFixed(2)}minutes `);
-      readline.moveCursor(process.stdout, 0, -1);
-    });
-    video.on('end', () => {
-      alert('Downloaded succefully')
-    });
+
+
+          this.video.on('progress', (chunckLength, downloaded, total) => {
+          const percent = downloaded / total;
+          const downloadedMinutes = (Date.now() - starttime) / 1000 / 60;
+          const estimatedDownloadTime = (downloadedMinutes / percent) - downloadedMinutes;
+          this.setState({
+            progress: (percent * 100).toFixed(2),
+            estTime: estimatedDownloadTime.toFixed(2),
+            downloadedSize: (downloaded / 1024 / 1024).toFixed(2),
+            totalSize: (total / 1024 / 1024).toFixed(2)
+          });
+          readline.cursorTo(process.stdout, 0);
+          process.stdout.write(`${(percent * 100).toFixed(2)}% downloaded `);
+          process.stdout.write(`(${(downloaded / 1024 / 1024).toFixed(2)}MB of ${(total / 1024 / 1024).toFixed(2)}MB)\n`);
+          process.stdout.write(`running for: ${downloadedMinutes.toFixed(2)}minutes`);
+          process.stdout.write(`, estimated time left: ${estimatedDownloadTime.toFixed(2)}minutes `);
+          readline.moveCursor(process.stdout, 0, -1);
+        });
+
+
+      this.video.on('end', () => {
+        alert('Downloaded succefully')
+      });
+
 
     // let video = ytdl(this.state.urlTxt.id,{format: 'mp4'});
     // video.pipe(fs.createWriteStream('video.mp4'));
@@ -78,7 +122,11 @@ class Mainview extends Component {
     return (
       <div>
         <input className="url" type="text" placeholder="Enter URL" onChange={this.inputChange} name="urlTxt" value={this.state.urlTxt} /> &nbsp;
-        <button className="paste" type="button" variant="outline-primary" onClick={() => this.download(this.state.urlTxt)}>Paste</button>
+        <button className="paste" type="button" variant="outline-primary" onClick={() => this.btnClick('download')}>Download</button>
+        <button className="paste" type="button" variant="outline-primary" onClick={() => this.pause('pause')}>Pause</button>
+        <button className="paste" type="button" variant="outline-primary" onClick={() => this.resume('resume')}>Resume</button>
+        <button className="paste" type="button" variant="outline-primary" onClick={() => this.stop('stop')}>Stop</button>
+        <button className="paste" type="button" variant="outline-primary" onClick={() => this.btnClick('msg')}>Msg</button>
         {this.state.progress && <p>{this.state.progress}% downloaded ({this.state.downloadedSize}MB of {this.state.totalSize}MB)</p>}
         {this.state.estTime && <p>Estimated time: {this.state.estTime} Minutes</p>}
       </div>
